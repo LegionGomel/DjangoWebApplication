910791d90359a0bc99f0e7bd319c089ef44d0a95
Yuri <75138435+LegionGomel@users.noreply.github.com>
2021-02-01 20:18:16 +0300

    Add admin user, topic class to models

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Topic(models.Model):
+    """Theme, that user studies"""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return string representation of a model"""
+        return self.text
