be097bc1cfe6588be67277363dc16511dd569917
Yuri <75138435+LegionGomel@users.noreply.github.com>
2021-02-02 18:51:41 +0300

    Add functions for edit existing entry

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -11,6 +11,8 @@ urlpatterns = [
     url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
     # Page for add a new topic
     url(r'^new_topic/$', views.new_topic, name='new_topic'),
-    # Page to add a new entry
+    # Page for add a new entry
     url(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry, name='new_entry'),
+    # Page for edit entry
+    url(r'^edit_entry/(?P<entry_id>\d+)/$', views.edit_entry, name='edit_entry'),
 ]
