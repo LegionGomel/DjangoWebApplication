3dd8d77459ebc825d1aa773c3f635bc8f26d013a
Yuri <75138435+LegionGomel@users.noreply.github.com>
2021-02-02 16:03:03 +0300

    Add single topic functions

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     # All topics output
     url(r'^topics/$', views.topics, name='topics'),
+    # Page with extended info about selected topic
+    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
 ]
