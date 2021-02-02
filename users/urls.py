7eaf512b9d65a534eebf12079a54a0967eb06e7e
Yuri <75138435+LegionGomel@users.noreply.github.com>
2021-02-02 19:24:46 +0300

    Create register function

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 """Define URL schemes for users"""
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, TemplateView
 from . import views
 
 app_name = "users"
@@ -10,4 +10,6 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='users/login.html'), name="login"),
     # Logout page
     path('logout/', LogoutView.as_view(template_name='users/logout.html'), name="logout"),
+    # Register page
+    path('register/', TemplateView.as_view(template_name='users/register.html'), name="register"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from django.contrib.auth import logout
+from django.contrib.auth import logout, login, authenticate
 from django.urls import reverse
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 
@@ -10,3 +11,23 @@ def logout_view(request):
     logout(request)
     return HttpResponseRedirect(reverse('learning_logs:index'))
 
+
+def register(request):
+    """Register new user"""
+    if request.method != 'POST':
+        # Display blank registration form.
+        form = UserCreationForm()
+    else:
+        # Handle filled form
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            new_user = form.save()
+            # Execute login and route to homepage
+            authenticated_user = authenticate(username=new_user.username,
+            password=request.POST['password1'])
+            login(request, authenticated_user)
+            return HttpResponseRedirect(reverse('learning_logs:index'))
+
+        context = {'form': form}
+        return render(request, 'users/register.html', context)
