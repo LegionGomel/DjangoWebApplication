2e252d7c94ec403197c6b97ba2284fcacbb65b9a
Yuri <75138435+LegionGomel@users.noreply.github.com>
2021-02-02 19:18:10 +0300

    Create logout function

--- users/urls.py
+++ users/urls.py
@@ -1,15 +1,13 @@
 """Define URL schemes for users"""
 from django.urls import path
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 app_name = "users"
+
 urlpatterns = [
     # Login page
-    path('login/',
-         LoginView.as_view(
-             template_name='users/login.html'
-         ),
-         name="login"
-         ),
+    path('login/', LoginView.as_view(template_name='users/login.html'), name="login"),
+    # Logout page
+    path('logout/', LogoutView.as_view(template_name='users/logout.html'), name="logout"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
+from django.contrib.auth import logout
+from django.urls import reverse
 # Create your views here.
+
+
+def logout_view(request):
+    """End user session"""
+    logout(request)
+    return HttpResponseRedirect(reverse('learning_logs:index'))
+
